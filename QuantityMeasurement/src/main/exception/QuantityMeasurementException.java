9cf100f57f52e9dc5549fed991a3cbe2754ef232
BL-Eswarraj <eswarraj.rajendiren@bridgelabz.com>
2026-06-29 12:32:21 +0530

    Eswar Added UC15: N Tier Architecture Refactor

---
+++ QuantityMeasurement/src/main/exception/QuantityMeasurementException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class QuantityMeasurementException
+        extends RuntimeException {
+
+    public QuantityMeasurementException(String message) {
+        super(message);
+    }
+}
